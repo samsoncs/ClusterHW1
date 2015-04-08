db90208ad7a21c055d383a019118c5b01e488f5e
samsoncs <samsoncs@stud.ntnu.no>
2015-04-08 13:45:11 -0700

    added some javadoc

--- src/api/Computer.java
+++ src/api/Computer.java
@@ -2,11 +2,22 @@ package api;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * This interface exposes the methods that can be called by the client
+ * @author Samson Svendsen, Simen Aakhus, Eivind Kristoffersen, Anders Kristiansen
+ *
+ */
 public interface Computer extends Remote{
 	 
 	public static String SERVICE_NAME = "ComputerService";
 	public static int PORT = 1099;
 	
+	/**
+	 * This remote method executes a certain task on the compute server
+	 * @param t the task that should be executed
+	 * @return returns a task T
+	 * @throws RemoteException can throw RemoteException
+	 */
 	<T> T execute(Task<T> t) throws RemoteException;
 	
 }
